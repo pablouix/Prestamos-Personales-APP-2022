d855397197123375fdf27fb12838a257c58ea822
pablouix <pablouix@gmail.com>
2022-10-12 19:40:14 -0400

    Reparando error

--- app/src/main/java/edu/ucne/prestamospersonales/di/AppModule.kt
+++ app/src/main/java/edu/ucne/prestamospersonales/di/AppModule.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
+
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,6 +14,7 @@ import dagger.hilt.components.SingletonComponent
 import edu.ucne.prestamospersonales.data.AppDataBase
 import edu.ucne.prestamospersonales.data.remota.ArticulosApi
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 //import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -58,7 +60,7 @@ object AppModule {
     fun provideSagApi(moshi: Moshi): ArticulosApi {
         return Retrofit.Builder()
             .baseUrl("http://http://www.apipablo.somee.com/")
-          //  .addConverterFactory(MoshiConverterFactory.create(moshi))
+             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(ArticulosApi::class.java)
     }
